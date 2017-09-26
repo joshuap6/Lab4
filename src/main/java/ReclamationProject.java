8ae7c1d274016fdff9eb30f3b50ae1c483ca2947
Josh <Josh@Joshua>
2017-09-26 14:39:46 -0500

    Fixed javadoc errors, made better variable names, and made it readable.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,37 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * Finds the longest common substring of characters.
+ */
+public class ReclamationProject {
+    /**
+     *Finds out what the longest substring is among two strings.
+     * @param firstInput the first string
+     * @param secondInput the second string
+     * @return r
+     */
+    static String doit(final String firstInput, final String secondInput) {
+        String firstString = firstInput;
+        String secondString = secondInput;
+        if (firstString.length() > secondString.length()) {
+            String tempString = firstString;
+            firstString = secondString;
+            secondString = tempString;
+            }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < firstString.length(); i++) {
+            for (int j = firstString.length() - i; j > 0; j--) {
+                for (int k = 0; k < secondString.length() - j; k++) {
+                    if (firstString.regionMatches(i, secondString, k, j) && j > r.length()) {
+                        r = firstString.substring(i, i + j);
+                        }
+                    }
+                        }
+                }
+        return r;
+        }
 }
